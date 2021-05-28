5a541d58159ef3371fd03686459bfd44b8489d27
Divyanshu Rawat <divyanshu.19r@gmail.com>
2021-05-28 14:39:38 +0530

    Added Walls Functionality to Grid

--- src/Components/Node/Node.js
+++ src/Components/Node/Node.js
@@ -1,9 +1,33 @@
 import React from "react";
 import "./Node.css";
 
-const Node = ({ isStart, isEnd, row, col }) => {
-  const classes = isStart ? "start-node" : isEnd ? "end-node" : "";
-  return <div className={`node ${classes}`} id={`node-${row}-${col}`}></div>;
+const Node = (props) => {
+  const {
+    row,
+    col,
+    isFinish,
+    isStart,
+    isWall,
+    onMouseDown,
+    onMouseEnter,
+    onMouseUp,
+  } = props;
+  const classes = isStart
+    ? "start-node"
+    : isFinish
+    ? "end-node"
+    : isWall
+    ? "wall-node"
+    : "";
+  return (
+    <div
+      className={`node ${classes}`}
+      id={`node-${row}-${col}`}
+      onMouseDown={() => onMouseDown(row, col)}
+      onMouseEnter={() => onMouseEnter(row, col)}
+      onMouseUp={() => onMouseUp()}
+    ></div>
+  );
 };
 
 export default Node;
--- src/Components/PathFind/PathFind.js
+++ src/Components/PathFind/PathFind.js
@@ -7,13 +7,14 @@ const cols = 40;
 const rows = 18;
 
 const NODE_START_ROW = 0;
-const NODE_START_COl = 0;
+const NODE_START_COL = 0;
 
 const NODE_END_ROW = rows - 1;
 const NODE_END_COL = cols - 1;
 
 const PathFind = () => {
   const [Grid, setGrid] = useState([]);
+  const [mouseIsPressed, setMouseIsPressed] = useState(false);
 
   useEffect(() => {
     initializeGrid();
@@ -41,28 +42,50 @@ const PathFind = () => {
 
   // Spot Constructor
   function Spot(i, j) {
-    this.x = i;
-    this.y = j;
-    this.isStart = this.x === NODE_START_ROW && this.y === NODE_START_COl;
-    this.isEnd = this.x === NODE_END_ROW && this.y === NODE_END_COL;
-    this.g = 0;
-    this.f = 0;
-    this.h = 0;
+    this.row = i;
+    this.col = j;
+    this.isStart = this.row === NODE_START_ROW && this.col === NODE_START_COL;
+    this.isFinish = this.row === NODE_END_ROW && this.col === NODE_END_COL;
+    this.distance = Infinity;
+    this.isVisited = false;
+    this.isWall = false;
+    this.previousNode = null;
   }
+
+  const handleMouseDown = (row, col) => {
+    const newGrid = getNewGridWithWallToggled(Grid, row, col);
+    setGrid(newGrid);
+    setMouseIsPressed(true);
+  };
+
+  const handleMouseEnter = (row, col) => {
+    if (!mouseIsPressed) return;
+    const newGrid = getNewGridWithWallToggled(Grid, row, col);
+    setGrid(newGrid);
+  };
+
+  const handleMouseUp = () => {
+    setMouseIsPressed(false);
+  };
 
   const gridWithNode = (
     <div>
       {Grid.map((row, rowIdx) => (
         <div key={rowIdx} className="rowWrapper">
           {row.map((col, colIdx) => {
-            const { isStart, isEnd } = col;
+            const { isStart, isFinish, isWall } = col;
             return (
               <Node
                 key={colIdx}
                 isStart={isStart}
-                isEnd={isEnd}
+                isFinish={isFinish}
+                isWall={isWall}
                 row={rowIdx}
                 col={colIdx}
+                mouseIsPressed={mouseIsPressed}
+                onMouseDown={(row, col) => handleMouseDown(row, col)}
+                onMouseEnter={(row, col) => handleMouseEnter(row, col)}
+                onMouseUp={() => handleMouseUp()}
               />
             );
           })}
@@ -71,9 +94,21 @@ const PathFind = () => {
     </div>
   );
 
+  const getNewGridWithWallToggled = (grid, row, col) => {
+    const newGrid = grid.slice();
+    const node = newGrid[row][col];
+    const newNode = {
+      ...node,
+      isWall: !node.isWall,
+    };
+    newGrid[row][col] = newNode;
+    console.log(newGrid);
+    return newGrid;
+  };
+
   return (
     <div className="wrapper">
-      <h1>Path Find Component</h1>
+      <h1>Path Finding Algorithm Visualizer</h1>
       {gridWithNode}
     </div>
   );
